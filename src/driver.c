9fb85c566ebc4b769d5f0df8fbe6d2678a336910
Zander Work <zander@zanderwork.com>
2020-03-13 16:44:38 -0700

    removed printing to screen

--- src/driver.c
+++ src/driver.c
@@ -2,9 +2,7 @@
 
 int main(int argc, char **argv) {
     TSP *tsp = init_tsp(argv[1]);
-    printf("TSP 0: %d %d\n", tsp->nodes[0].x, tsp->nodes[0].y);
-    printf("TSP %d: %d %d\n", tsp->num_nodes-1, tsp->nodes[tsp->num_nodes-1].x, tsp->nodes[tsp->num_nodes-1].y);
-
+    
     // get euclidean distances for graph
     generate_graph(tsp);
 
@@ -13,7 +11,6 @@ int main(int argc, char **argv) {
 
     // get the odd-degree vertices
     array_t odd_vertices = get_odd_vertices(tsp);
-    printf("There are %d odd vertices\n", odd_vertices->length);
 
     // build the perfect matching graph
     build_perfect_matching(tsp, odd_vertices);
